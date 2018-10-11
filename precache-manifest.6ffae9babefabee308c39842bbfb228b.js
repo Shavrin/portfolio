199d2c1a4f6fbabd9a6fa86d22dbd23bd41010a4
Kacper Olek <kacper.olek@gmail.com>
2018-10-11 09:29:22 +0200

    Updates

---
+++ precache-manifest.6ffae9babefabee308c39842bbfb228b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "4bfcab1d1e6bd161e818",
+    "url": "/portfolio/static/js/main.4bfcab1d.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/portfolio/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "4bfcab1d1e6bd161e818",
+    "url": "/portfolio/static/css/main.4e4f1f55.chunk.css"
+  },
+  {
+    "revision": "a23ce0fd8c8b0130f71be0583d5b7208",
+    "url": "/portfolio/index.html"
+  }
+];
